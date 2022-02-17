c711749158d31a23fbd2169431e46333360cb478
Adam160 <99061415+Adam160@users.noreply.github.com>
2022-02-17 02:04:59 -0500

    Enter Number / Display outputs

---
+++ P1HW1_ParksDaniel.py
@@ -0,0 +1,17 @@
+print('Enter your first number.', end=' ')
+Number1 = int(input())
+print('Your first number is:', Number1)
+print('Enter your second number.', end=' ')
+Number2 = int(input())
+print('Your second number is:', Number2)
+
+NumberSum = (Number1 + Number2)
+NumberProduct = (Number1 * Number2)
+
+print('The sum of your numbers is:', NumberSum )
+print('The product of your numbers is:', NumberProduct )
+print(Number1)
+print(Number2)
+print(NumberSum)
+print(NumberProduct)
+
