44bcf375aad3854541e7bb5f9958be97b64904f0
Adam160 <99061415+Adam160@users.noreply.github.com>
2022-02-17 01:56:12 -0500

    Add files via upload
    Pizza - Pseudocode
    
    Begin
    Output = ‘How many students do you want to order pizza for?’
    Input = Variable; NumberOfStudents
    Claculate; NumberOfStudents * 3 = NumberOfSlices
    Calculate; NumberOfStudents / 2 = NumberOfPizzas
    Output = '----Pizza Order--------'
    Output = 'Number of Students :', NumberOfStudents
    Output = 'Pizza Slices Needed:', NumberOfSlices
    Output = 'Pizzas Needed : ', NumberOfPizzas
    Output = '-----------------------'
    END

---
+++ P1HW2_ParksDaniel.py
@@ -0,0 +1,9 @@
+print('How many students do you want to order pizza for?')
+NumberOfStudents = int(input())
+NumberOfSlices = NumberOfStudents*3
+NumberOfPizzas = NumberOfStudents/2
+print('----Pizza Order--------')
+print('Number of Students :', NumberOfStudents)
+print('Pizza Slices Needed:', NumberOfSlices)
+print('Pizzas Needed : ', NumberOfPizzas)
+print('-----------------------')
