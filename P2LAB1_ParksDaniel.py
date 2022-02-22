1d07ab067592c0d259a1c62d0738bbb4b95095ea
Adam160 <99061415+Adam160@users.noreply.github.com>
2022-02-21 19:54:42 -0500

    Working with Variables.
    Working with Variables.

---
+++ P2LAB1_ParksDaniel.py
@@ -0,0 +1,13 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+
+user_int2 = float(input('Enter float:\n'))
+
+user_int3 = str(input('Enter character:\n'))
+
+user_int4 = str(input("Enter string:\n"))
+
+print(user_int, user_int2, user_int3, user_int4)
+
+print(user_int4, user_int3, user_int2, user_int)
+
+print(user_int, "converted to a character is " + chr(user_int))
